e953c53de4dbc699e43adc11744ee92dc7a6bb52
Alexandr Kulinkovich <alexandr_kulinkovich@icloud.com>
2021-07-12 19:52:54 +0300

    [BE] API request for goods

--- be/src/services/goods.ts
+++ be/src/services/goods.ts
@@ -1,20 +1,59 @@
 import { Request, Response } from "express";
 import { GoodModel } from "../models/good";
 
-export async function getGoodById(_: Request, res: Response) {
-  const item = await GoodModel.findOne();
+export async function getGoodById(req: Request, res: Response) {
+  const item = await GoodModel.findById(req.params.id);
   res.send(item);
 }
 
-export async function createGood(_: Request, res: Response) {
-  const model = new GoodModel({
-    title: "Abc",
-    description: "asdasdasdasdasd",
-    category: "123",
-    imgSrc: "1",
-    price: 1000,
+export async function getAllGood(_: Request, res: Response) {
+  const item = await GoodModel.find();
+  res.send(item);
+}
+
+export async function updateGood(req: Request, res: Response) {
+
+  const item: any = await GoodModel.findById(req.params.id);
+
+  item.title = req.body.title;
+  item.description = req.body.description;
+  item.category = req.body.category;
+  item.imgSrc = req.body.imgSrc;
+  item.price = req.body.price;
+
+  await item.save();
+  res.send(item);
+}
+
+export async function createGood(req: Request, res: Response) {
+
+  const item: any = new GoodModel({
+    title: req.body.title,
+    description: req.body.description,
+    category: req.body.category,
+    imgSrc: req.body.imgSrc,
+    price: req.body.price,
     isDelete: false,
   });
-  const result = await model.save();
-  res.send(result);
+
+  await item.save();
+  res.send(item);
+}
+
+export async function deleteGood(req: Request, res: Response) {
+
+  const item: any = await GoodModel.findById(req.params.id);
+  await item.deleteOne();
+  res.send(item);
+}
+
+
+export async function hideGood(req: Request, res: Response) {
+
+  const item: any = await GoodModel.findById(req.params.id);
+
+  item.isDelete = req.body.isDelete;
+
+  await item.save();
+  res.send(item);
 }
