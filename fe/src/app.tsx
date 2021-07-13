1747912d2327e60ace95ee07e078347c99721d2c
Alexandr Kulinkovich <alexandr_kulinkovich@icloud.com>
2021-07-13 14:45:58 +0300

    [FE] Create component Header and pages

--- fe/src/app.tsx
+++ fe/src/app.tsx
@@ -1,46 +1,7 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import { BasketPage } from "./pages/basket";
-import { HomePage } from "./pages/home";
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles({
-  list: {
-    width: 250,
-  },
-  fullList: {
-    width: 'auto',
-  },
-});
-
+import {Header} from "./components/Header";
 
 export const App = () => {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/basket">Basket</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/basket">
-            <BasketPage />
-          </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <Header/>
   );
 }
